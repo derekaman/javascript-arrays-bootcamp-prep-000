a86019600b29734e9f4e963a82c3304e1a1297f9
Derek Aman <derekaman@gmail.com>
2018-04-06 19:43:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
   
 function removeElementFromBeginningOfArray(array) {
-  var newArray = array.slice();
+  newArray = array.slice();
   return newArray;
 }
   

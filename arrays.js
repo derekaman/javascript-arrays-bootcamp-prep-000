ad013877fb05e07d9e6bce5d9d798785dbb8f5d4
Derek Aman <derekaman@gmail.com>
2018-04-06 18:36:47 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,36 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+ var newArray = [element, ...array];
+ return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+ array.unshift(element);
+  return array;
+  }
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+ array.push(element);
+  return array;
+}
+
+//function accessElementInArray(array, index) {
+  //console.log(array[index]);
+  //return index;
+  //}
+  
+  //function destructivelyRemoveElementFromBeginningOfArray(array) {
+    //array.shift(index);
+    //return array;
+  //}
+  
+  function removeElementFromBeginningOfArray(array) {
+    var newArray = array.slice(index);
+    return newArray
+  }
